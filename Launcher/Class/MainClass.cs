097d7d9bb2d4f0d8fcaff5a28097c611e9972172
Edin Jašarević <edinjasar14@gmail.com>
2017-08-23 16:17:05 +0200

    assambly info and entry point

--- Launcher/Class/MainClass.cs
+++ Launcher/Class/MainClass.cs
@@ -5,7 +5,7 @@ using System.Windows.Forms;
 
 namespace Jasarsoft.Launcher.SAMP
 {
-    static class Launcher
+    static class MainClass
     {
         /// <summary>
         /// The main entry point for the application.
