2c4c437082753adf8c4cefcf8413f6c98437b992
Edin Jašarević <edinjasar14@gmail.com>
2018-02-11 21:29:44 +0100

    title msg base class

---
+++ Launcher/Class/Title/MessageTitle.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Jasarsoft.Launcher.SAMP
+{
+    internal abstract class MessageTitle : IMessageTitle
+    {
+        private readonly string caption;
+        
+
+        //constructor
+        public MessageTitle()
+        {
+            this.caption = "SAMP - Launcher";
+        }
+
+
+        public virtual string Caption
+        {
+            get { return this.caption; }
+        }
+
+
+        public override string ToString()
+        {
+            return caption;
+        }
+    }
+}
