d95c8d45a2aa27dbef4abb8c3d71b9852af9c733
Edin Jašarević <edinjasar14@gmail.com>
2018-02-11 21:30:45 +0100

    hash class

---
+++ Launcher/Class/Other/HashFile.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+namespace Jasarsoft.Launcher.SAMP
+{
+    internal sealed class HashFile
+    {
+        private string fileName;
+
+
+        public HashFile()
+        {
+            fileName = null;
+        }
+
+        public HashFile(string filename)
+        {
+            this.fileName = filename;
+        }
+
+
+        public string FileName
+        {
+            get { return this.fileName; }
+            set { this.fileName = value; }
+        }
+
+
+        public string HashMD5()
+        {
+            if (fileName == null || !File.Exists(fileName)) return null;
+
+            using (MD5 hash = MD5.Create())
+            {
+                using (FileStream stream = File.OpenRead(fileName))
+                {
+                    return BitConverter.ToString(hash.ComputeHash(stream)).Replace("-", String.Empty);
+                }
+            }
+        }
+
+        public string HashSHA1()
+        {
+            if (fileName == null || !File.Exists(fileName)) return null;
+
+            using (SHA1 hash = SHA1.Create())
+            {
+                using (FileStream stream = File.OpenRead(fileName))
+                {
+                    return BitConverter.ToString(hash.ComputeHash(stream)).Replace("-", String.Empty);
+                }
+            }
+        }
+    }
+}
