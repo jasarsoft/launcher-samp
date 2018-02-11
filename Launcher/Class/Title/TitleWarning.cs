557bb5fa7a085f49fb3b4c88a262b40d89c1d951
Edin Jašarević <edinjasar14@gmail.com>
2018-02-11 21:30:21 +0100

    title warning

---
+++ Launcher/Class/Title/TitleWarning.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Jasarsoft.Launcher.SAMP
+{
+    internal sealed class TitleWarning : MessageTitle, IMessageTitle
+    {
+        //contructor
+        public TitleWarning() : base()
+        {
+            //nothing
+        }
+
+
+        public override string Caption
+        {
+            get
+            {
+                return String.Format("{0} | Upozorenje", base.Caption);
+            }
+        }
+
+
+        public override string ToString()
+        {
+            return base.Caption;
+        }
+    }
+}
