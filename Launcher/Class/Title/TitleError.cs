aa1cf8737c7a8022857e983a20c2760ec9db7744
Edin Jašarević <edinjasar14@gmail.com>
2018-02-11 21:29:57 +0100

    title error

---
+++ Launcher/Class/Title/TitleError.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Jasarsoft.Launcher.SAMP
+{
+    internal sealed class TitleError : MessageTitle, IMessageTitle
+    {
+        //default contructor
+        public TitleError() : base()
+        {
+            //noting
+        }
+
+
+        public override string Caption
+        {
+            get
+            {
+                return String.Format("{0} | Greška", base.Caption);
+            }
+        }
+
+
+        public override string ToString()
+        {
+            return base.Caption;
+        }
+    }
+}
