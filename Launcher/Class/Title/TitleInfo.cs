33ec3ae5d8078bd84983e0ee3d83d247402cec05
Edin Jašarević <edinjasar14@gmail.com>
2018-02-11 21:30:10 +0100

    title info

---
+++ Launcher/Class/Title/TitleInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Jasarsoft.Launcher.SAMP
+{
+    internal sealed class TitleInfo : MessageTitle, IMessageTitle
+    {
+        //contructor
+        public TitleInfo() : base()
+        {
+            //noting
+        }
+
+
+        public override string Caption
+        {
+            get
+            {
+                return String.Format("{0} | Info", base.Caption);
+            }
+        }
+
+
+        public override string ToString()
+        {
+            return base.Caption;
+        }
+    }
+}
