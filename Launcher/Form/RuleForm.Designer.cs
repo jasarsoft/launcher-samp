a868b9dc0dfac048e0feba9d2c331c38878fac39
Edin Jašarević <edinjasar14@gmail.com>
2018-04-01 14:46:30 +0200

    dodavanje novog comobox i ispravka indextabova

--- Launcher/Form/RuleForm.Designer.cs
+++ Launcher/Form/RuleForm.Designer.cs
@@ -37,12 +37,12 @@ namespace Jasarsoft.Launcher.SAMP
         {
             this.components = new System.ComponentModel.Container();
             this.pictureLogo = new System.Windows.Forms.PictureBox();
-            this.comboRule = new Syncfusion.Windows.Forms.Tools.ComboDropDown();
             this.textRule = new Syncfusion.Windows.Forms.Tools.TextBoxExt();
             this.buttonClose = new Syncfusion.Windows.Forms.ButtonAdv();
+            this.comboRule = new Syncfusion.Windows.Forms.Tools.ComboBoxAdv();
             ((System.ComponentModel.ISupportInitialize)(this.pictureLogo)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.comboRule)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.textRule)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.comboRule)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureLogo
@@ -51,29 +51,13 @@ namespace Jasarsoft.Launcher.SAMP
             this.pictureLogo.Location = new System.Drawing.Point(12, 12);
             this.pictureLogo.Name = "pictureLogo";
             this.pictureLogo.Size = new System.Drawing.Size(320, 96);
-            this.pictureLogo.TabIndex = 9;
+            this.pictureLogo.TabIndex = 0;
             this.pictureLogo.TabStop = false;
-            // 
-            // comboRule
-            // 
-            this.comboRule.BackColor = System.Drawing.Color.White;
-            this.comboRule.BeforeTouchSize = new System.Drawing.Size(320, 21);
-            this.comboRule.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboRule.FlatBorderColor = System.Drawing.Color.Gainsboro;
-            this.comboRule.ForeColor = System.Drawing.Color.Black;
-            this.comboRule.Location = new System.Drawing.Point(12, 123);
-            this.comboRule.MaxLength = 64;
-            this.comboRule.MetroBorderColor = System.Drawing.Color.Gainsboro;
-            this.comboRule.MetroColor = System.Drawing.Color.DarkCyan;
-            this.comboRule.Name = "comboRule";
-            this.comboRule.Size = new System.Drawing.Size(320, 21);
-            this.comboRule.Style = Syncfusion.Windows.Forms.VisualStyle.Metro;
-            this.comboRule.TabIndex = 1;
             // 
             // textRule
             // 
             this.textRule.BackColor = System.Drawing.Color.White;
-            this.textRule.BeforeTouchSize = new System.Drawing.Size(214, 22);
+            this.textRule.BeforeTouchSize = new System.Drawing.Size(320, 96);
             this.textRule.BorderColor = System.Drawing.Color.Gainsboro;
             this.textRule.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.textRule.FocusBorderColor = System.Drawing.Color.DarkCyan;
@@ -105,6 +89,26 @@ namespace Jasarsoft.Launcher.SAMP
             this.buttonClose.Text = "CLOSE";
             this.buttonClose.UseVisualStyle = true;
             this.buttonClose.UseVisualStyleBackColor = false;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // comboRule
+            // 
+            this.comboRule.BackColor = System.Drawing.Color.White;
+            this.comboRule.BeforeTouchSize = new System.Drawing.Size(320, 21);
+            this.comboRule.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboRule.FlatBorderColor = System.Drawing.Color.LightGray;
+            this.comboRule.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.comboRule.ForeColor = System.Drawing.Color.Black;
+            this.comboRule.Location = new System.Drawing.Point(12, 123);
+            this.comboRule.MaxDropDownItems = 7;
+            this.comboRule.MaxLength = 1024;
+            this.comboRule.MetroBorderColor = System.Drawing.Color.Gainsboro;
+            this.comboRule.MetroColor = System.Drawing.Color.DarkCyan;
+            this.comboRule.Name = "comboRule";
+            this.comboRule.Size = new System.Drawing.Size(320, 21);
+            this.comboRule.Style = Syncfusion.Windows.Forms.VisualStyle.Metro;
+            this.comboRule.TabIndex = 1;
+            this.comboRule.SelectedIndexChanged += new System.EventHandler(this.comboBox_SelectedIndexChanged);
             // 
             // RuleForm
             // 
@@ -122,9 +126,9 @@ namespace Jasarsoft.Launcher.SAMP
             this.CaptionForeColor = System.Drawing.Color.WhiteSmoke;
             this.ClientSize = new System.Drawing.Size(344, 341);
             this.ControlBox = false;
+            this.Controls.Add(this.comboRule);
             this.Controls.Add(this.buttonClose);
             this.Controls.Add(this.textRule);
-            this.Controls.Add(this.comboRule);
             this.Controls.Add(this.pictureLogo);
             this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.ForeColor = System.Drawing.Color.Black;
@@ -140,9 +144,10 @@ namespace Jasarsoft.Launcher.SAMP
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SAMP Launcher | Rules";
             this.TopMost = true;
+            this.Load += new System.EventHandler(this.RuleForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureLogo)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.comboRule)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.textRule)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.comboRule)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -151,8 +156,8 @@ namespace Jasarsoft.Launcher.SAMP
         #endregion
 
         private System.Windows.Forms.PictureBox pictureLogo;
-        private Syncfusion.Windows.Forms.Tools.ComboDropDown comboRule;
         private Syncfusion.Windows.Forms.Tools.TextBoxExt textRule;
         private Syncfusion.Windows.Forms.ButtonAdv buttonClose;
+        private Syncfusion.Windows.Forms.Tools.ComboBoxAdv comboRule;
     }
 }
