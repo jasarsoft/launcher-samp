fbc79b7a4784f02ef7d4088706362bbb69afe6f9
Edin Jašarević <edinjasar14@gmail.com>
2018-04-01 14:47:38 +0200

    uspostava za roleplay sadrzaja

--- Launcher/Form/RuleForm.cs
+++ Launcher/Form/RuleForm.cs
@@ -18,9 +18,33 @@ namespace Jasarsoft.Launcher.SAMP
 {
     public partial class RuleForm : Syncfusion.Windows.Forms.MetroForm
     {
+        RolePlayBS rolePlay;
+
         public RuleForm()
         {
             InitializeComponent();
+
+            rolePlay = new RolePlayBS();
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void RuleForm_Load(object sender, EventArgs e)
+        {
+            comboRule.Items.Clear();
+
+            for (int i = 0; i < rolePlay.Lenght; i++)
+            {
+                comboRule.Items.Add(rolePlay[i]);
+            }
+        }
+
+        private void comboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textRule.Text = ((RoleRule)comboRule.SelectedItem).Info;
         }
     }
 }
