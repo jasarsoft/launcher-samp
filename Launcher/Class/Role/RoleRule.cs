b652edd428cacf6f3edd7438694796752be1361a
Edin Jašarević <edinjasar14@gmail.com>
2018-04-01 14:48:23 +0200

    rolerule klasa objekta

---
+++ Launcher/Class/Role/RoleRule.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Jasarsoft.Launcher.SAMP
+{
+    internal sealed class RoleRule
+    {
+        private readonly string name;
+        private readonly string title;
+        private readonly string info;
+
+
+        public RoleRule(string name, string title, string info)
+        {
+            this.name = name;
+            this.title = title;
+            this.info = info;
+        }
+
+
+        public string Name
+        {
+            get { return this.name; }
+        }
+
+        public string Title
+        {
+            get { return this.title; }
+        }
+
+        public string Info
+        {
+            get { return this.info; }
+        }
+
+
+        public override string ToString()
+        {
+            return String.Format("{0} - {1}", name, title);
+        }
+    }
+}
