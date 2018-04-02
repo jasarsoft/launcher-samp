3b671e631307dcc711a48ab446b28f734f083a15
Edin Jašarević <edinjasar14@gmail.com>
2018-04-02 19:31:14 +0200

    server opcode struct

---
+++ Launcher/Structure/Server/ServerOpcode.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Jasarsoft.Launcher.SAMP
+{
+    struct ServerOpcode
+    {
+        public const char INFO = 'i';
+        public const char RULE = 'r';
+        public const char CLIENT = 'c';
+        public const char PLAYER = 'd';
+        public const char RCON = 'x';
+        public const char PING = 'p';
+    }
+}
