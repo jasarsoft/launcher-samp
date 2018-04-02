03fdaa9c0e56967b013f943d80880f711a12b5be
Edin Jašarević <edinjasar14@gmail.com>
2018-04-02 19:31:03 +0200

    server ping

---
+++ Launcher/Class/Server/ServerPing.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Jasarsoft.Launcher.SAMP
+{
+    class ServerPing : ServerQuery
+    {
+        private int pingValue;      //ping milisecond value
+
+        public ServerPing(string ip, int port) : base(ip, port)
+        {
+            pingValue = Ping();
+        }
+
+        public int Value
+        {
+            get { return pingValue; }
+        }
+
+
+        public int Ping()
+        {
+            if(Send(ServerOpcode.PING))
+            {
+                pingValue = Convert.ToInt32(Result[0]);
+            }
+
+            return 0;
+        }
+    }
+}
