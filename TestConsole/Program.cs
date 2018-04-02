4e9f3a75bb8d39d68e7f7d4edf0aafef4e241929
Edin Jašarević <edinjasar14@gmail.com>
2018-04-02 15:11:57 +0200

    create a console test project

---
+++ TestConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
