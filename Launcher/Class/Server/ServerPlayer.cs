485d42add0842e10d2fa08cc457d03f397563821
Edin Jašarević <edinjasar14@gmail.com>
2018-04-03 17:43:13 +0200

    server player receive2

--- Launcher/Class/Server/ServerPlayer.cs
+++ Launcher/Class/Server/ServerPlayer.cs
@@ -34,6 +34,52 @@ namespace Jasarsoft.Launcher.SAMP
             return false;
         }
 
+        private bool Receive2()
+        {
+            byte[] buffer = new byte[3402];
+            if(Receive(ref buffer))
+            {
+                using (MemoryStream stream = new MemoryStream(buffer))
+                {
+                    using (BinaryReader reader = new BinaryReader(stream))
+                    {
+                        if (stream.Length <= 10)
+                            return false;
+                        else
+                            reader.ReadBytes(10);
+
+                        if (reader.ReadChar() == ServerOpcode.PLAYER)
+                        {
+                            int playercount = reader.ReadInt16();
+
+                            for (int i = 0; i < playercount; i++)
+                            {
+                                //int playerid = reader.ReadByte();
+                                //int playernicklen = reader.ReadByte();
+                                //string playernick = new string(reader.ReadChars(playernicklen));
+                                //int playerscore = reader.ReadInt32();
+                                //int playerping = reader.ReadInt32();
+
+                                //playersInfo.Add(new PlayerInfo(playerid, playernick, playerscore, playerping));
+
+
+                                playersInfo.Add(new PlayerInfo(reader.ReadByte(),
+                                                               new string(reader.ReadChars(reader.ReadByte())),
+                                                               reader.ReadInt32(),
+                                                               reader.ReadInt32()));
+
+                            }
+
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            return false;
+            
+        }
+
         private new bool Receive()
         {
             byte[] buffer = new byte[3402];
