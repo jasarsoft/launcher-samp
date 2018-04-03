ed16affdecc52cd3bfd220c61a24bcf071458940
Edin Jašarević <edinjasar14@gmail.com>
2018-04-03 17:19:50 +0200

    new player info object

---
+++ Launcher/Class/Player/PlayerInfo.cs
@@ -0,0 +1,49 @@
+﻿
+namespace Jasarsoft.Launcher.SAMP
+{
+    internal sealed class PlayerInfo
+    {
+        private int playerID;
+        private string playerNick;
+        private int playerScore;
+        private int playerPing;
+
+        
+        public PlayerInfo(int id, string nick, int score)
+        {
+            this.playerID = id;
+            this.playerNick = nick;
+            this.playerScore = score;
+            this.playerPing = 0;
+        }
+
+        public PlayerInfo(int id, string nick, int score, int ping)
+        {
+            this.playerID = id;
+            this.playerNick = nick;
+            this.playerScore = score;
+            this.playerPing = ping;
+        }
+
+
+        public int ID
+        {
+            get { return playerID; }
+        }
+
+        public string Nick
+        {
+            get { return playerNick; }
+        }
+
+        public int Score
+        {
+            get { return playerScore; }
+        }
+
+        public int Ping
+        {
+            get { return playerPing; }
+        }
+    }
+}
