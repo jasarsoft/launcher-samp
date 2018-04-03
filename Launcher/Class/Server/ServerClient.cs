1dba885271c8b42c247b4aa03b464498bc8c685d
Edin Jašarević <edinjasar14@gmail.com>
2018-04-03 21:31:24 +0200

    server client class

---
+++ Launcher/Class/Server/ServerClient.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Jasarsoft.Launcher.SAMP
+{
+    internal sealed class ServerClient : ServerQuery
+    {
+        PlayerClient playerClient;
+        
+        public ServerClient(string ip, int port) : base(ip, port)
+        {
+            //nothing
+        }
+
+
+        public PlayerClient Player
+        {
+            get { return playerClient; }
+        }
+
+
+        public bool Client()
+        {
+            return Send(OpcodeKey.CLIENT) && Receive() ? true : false;
+        }
+
+        private new bool Receive()
+        {
+            byte[] buffer = new byte[128];
+
+            if (base.Receive(ref buffer))
+            {
+                using (MemoryStream stream = new MemoryStream(buffer))
+                {
+                    using (BinaryReader reader = new BinaryReader(stream))
+                    {
+                        if (stream.Length <= 10)
+                            return false;
+                        else
+                            reader.ReadBytes(10);
+#if DEBUG
+                        string debug = String.Format("{0}: Binary stream size {1}",
+                                                      this.ToString(), stream.Length);
+
+                        System.Diagnostics.Debug.WriteLine(debug);
+#endif
+
+                        if (reader.ReadChar() == OpcodeKey.CLIENT)
+                        {
+                            int playercount = reader.ReadInt16();
+
+#if DEBUG
+                            System.Diagnostics.Debug.Write(String.Format("{0}: Player count: {1}", this.ToString(), playercount));
+#endif
+
+                            for (int i = 0; i < playercount; i++)
+                            {
+                                int namelen = reader.ReadByte();
+                                string name = new string(reader.ReadChars(namelen));
+
+                                int score = reader.ReadInt32();
+
+                                playerClient = new PlayerClient(name, score);
+                            }
+
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
