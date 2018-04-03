4b14d75afb3182f8c9930ee02f802fa5acd3cdad
Edin Jašarević <edinjasar14@gmail.com>
2018-04-03 21:15:42 +0200

    add player client class

---
+++ Launcher/Class/Player/PlayerClient.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Jasarsoft.Launcher.SAMP
+{
+    internal sealed class PlayerClient
+    {
+        private string playerNick;
+        private int playerScore;
+
+
+        public PlayerClient(string nick, int score)
+        {
+            playerNick = nick;
+            playerScore = score;
+        }
+
+
+        public string Nick
+        {
+            get { return playerNick; }
+        }
+
+        public int Score
+        {
+            get { return playerScore; }
+        }
+    }
+}
