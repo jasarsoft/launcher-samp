1af95ea37b70746637aa6730f47db89d7f7ac9a6
Edin Jašarević <edinjasar14@gmail.com>
2018-04-19 22:44:11 +0200

    server ip class

---
+++ Launcher/Class/Server/ServerIp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Jasarsoft.Launcher.SAMP
+{
+    internal sealed class ServerIp
+    {
+        private static string ip;
+        private static int port;
+
+        public ServerIp(string _ip, int _port)
+        {
+            ip = _ip;
+            port = _port;
+        }
+
+
+        public string Ip
+        {
+            get { return ip; }
+        }
+
+        public int Port
+        {
+            get { return port; }
+        }
+
+        public string Address
+        {
+            get { return String.Format("{0}:{1}", ip, port); }
+        }
+    }
+}
