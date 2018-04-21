ed580ba044cb4aab594fa5a0ba9f2e1900e8d297
Edin Jašarević <edinjasar14@gmail.com>
2018-04-21 13:17:38 +0200

    create a userfile

---
+++ Launcher/Class/User/UserFile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Jasarsoft.Launcher.SAMP
+{
+    class UserFile
+    {
+        private char[] fileHeader;
+        private int fileVersion;
+        private int fileLength;
+        private List<UserServer> fileServers;
+
+
+        public UserFile()
+        {
+
+        }
+    }
+}
