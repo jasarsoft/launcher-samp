b839a6d9a59b485e83ada3e7657b26ffc8ba2051
Edin Jašarević <edinjasar14@gmail.com>
2018-04-21 13:16:47 +0200

    user file server

---
+++ Launcher/Class/User/UserServer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+
+namespace Jasarsoft.Launcher.SAMP
+{
+    sealed class UserServer
+    {
+        private string userAddress;
+        private int userPort;
+        private string userHostname;
+        private string userPassword;
+        private string userRcon;
+
+
+        public UserServer(string ip, int port)
+        {
+            this.userAddress = ip;
+            this.userPort = port;
+        }
+
+        public UserServer(string ip, int port, string host) : this(ip, port)
+        {
+            this.userHostname = host;
+        }
+
+        public UserServer(string ip, int port, string host, string password) : this(ip, port, host)
+        {
+            this.userPassword = password;
+        }
+
+        public UserServer(string ip, int port, string host, string password, string rcon) : this(ip, port, host, password)
+        {
+            this.userRcon = rcon;
+        }
+
+
+        public string Address
+        {
+            get { return userAddress; }
+        }
+
+        public int Port
+        {
+            get { return userPort; }
+        }
+
+        public string Hostname
+        {
+            get { return userHostname; }
+        }
+
+        public string Password
+        {
+            get { return userPassword; }
+        }
+
+        public string Rcon
+        {
+            get { return userRcon; }
+        }
+    }
+}
