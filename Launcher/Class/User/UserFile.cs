9380d5c6191dafa5daa1e4431f5913c977355cdb
Edin Jašarević <edinjasar14@gmail.com>
2018-04-21 13:54:03 +0200

    user file class

--- Launcher/Class/User/UserFile.cs
+++ Launcher/Class/User/UserFile.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -12,10 +13,53 @@ namespace Jasarsoft.Launcher.SAMP
         private int fileLength;
         private List<UserServer> fileServers;
 
+        private readonly string filePath;
 
         public UserFile()
         {
+            this.filePath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + 
+                            @"\GTA San Andreas User Files\SAMP\USERDATA.DAT";
+        }
 
+
+        public UserServer[] Servers
+        {
+            get { return fileServers.ToArray(); }
+        }
+
+
+        public void Read()
+        {
+            fileServers = new List<UserServer>();
+
+            if (!File.Exists(filePath)) return;
+
+            using (BinaryReader reader = new BinaryReader(File.Open(filePath, FileMode.Open)))
+            {
+                fileHeader = reader.ReadChars(4);   //header
+                fileVersion = reader.ReadInt32();   //file version
+                fileLength = reader.ReadInt32();    //server count
+
+                for (int i = 0; i < fileLength; i++)
+                {
+                    //address
+                    int addresslen = reader.ReadInt32();
+                    string address = reader.ReadChars(addresslen).ToString();
+                    //port
+                    int port = reader.ReadInt32();
+                    //hostname
+                    int hostlen = reader.ReadInt32();
+                    string host = reader.ReadChars(hostlen).ToString();
+                    //password
+                    int pwlen = reader.ReadInt32();
+                    string pw = reader.ReadChars(pwlen).ToString();
+                    //rcon
+                    int rconlen = reader.ReadInt32();
+                    string rcon = reader.ReadChars(rconlen).ToString();
+
+                    fileServers.Add(new UserServer(address, port, host, pw, rcon));
+                }
+            }
         }
     }
 }
